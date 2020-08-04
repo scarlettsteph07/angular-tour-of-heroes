c24a1e379cde1593161a4a32af261b74a36ac0b2
Stephanie Scarlett <scarlets@uw.edu>
2020-08-04 16:55:27 -0700

    Code clean up: prettier

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,4 +1,8 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+} from '@angular/core/testing';
 
 import { HeroesComponent } from './heroes.component';
 
