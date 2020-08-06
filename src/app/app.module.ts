10f32082d81c899629960303dfb699bbf694d8bb
Stephanie Scarlett <scarlets@uw.edu>
2020-08-06 09:00:18 -0700

    Update MessagesComponent template styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -42,7 +44,9 @@ import { MessagesComponent } from './messages/messages.component';
 
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatIconModule,
+    MatListModule,
     MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
